6fd0835bfe0e12dff8e26b2a100a0b117af78d0a
SethJPistol <sethjpistol@gmail.com>
2020-05-04 14:43:05 +1000

    Farmer AI moves towards closest nest

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+	private Transform m_seekTarget;
+	private NavMeshAgent m_agent;
+	private Nest[] m_nests;
+
+    void Start()
+    {
+		m_agent = GetComponent<NavMeshAgent>();
+
+		GameObject[] nestObjects = GameObject.FindGameObjectsWithTag("Nest");
+		m_nests = new Nest[nestObjects.Length];
+		for (int i = 0; i < nestObjects.Length; ++i)
+			m_nests[i] = nestObjects[i].GetComponent<Nest>();
+	}
+
+    void Update()
+    {
+        if (!m_agent.hasPath)
+		{
+			//If not at a nest
+			Nest closestNest = ClosestNest();
+			m_agent.SetDestination(closestNest.transform.position);
+			//If at the nest
+		}
+    }
+
+	private Nest ClosestNest()
+	{
+		bool first = true;
+		Nest closestNest = null;
+		float closestDistance = 0;
+
+		foreach (Nest nest in m_nests)
+		{
+			float distance = Vector3.Distance(nest.transform.position, transform.position);
+
+			if (first)
+			{
+				closestDistance = distance;
+				closestNest = nest;
+				first = false;
+			}
+			else if (distance < closestDistance)
+			{
+				closestDistance = distance;
+				closestNest = nest;
+			}
+		}
+
+		return closestNest;
+	}
+}
---
+++ Assets/Scripts/Nest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nest : MonoBehaviour
+{
+	[HideInInspector]
+	public bool hasEgg;
+
+	private void Start()
+	{
+		hasEgg = true;
+	}
+}
