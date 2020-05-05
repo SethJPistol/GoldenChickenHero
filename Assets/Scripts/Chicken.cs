ee424ae3ff477e1a61e7d94517991946527fdcc6
Troy Rooney <troyrooney39@gmail.com>
2020-05-05 12:13:50 +1000

    Bug Fixes

--- Assets/Scripts/Chicken.cs
+++ Assets/Scripts/Chicken.cs
@@ -6,13 +6,11 @@ public class Chicken : MonoBehaviour
 
 
     //Position of the egg, when picked up by the chicken
-    [SerializeField] Transform m_HoldTransform;
+    [SerializeField] Transform m_HoldTransform = null;
 
     //The radius of the sphere cast
     [SerializeField] float m_Radius = 1.0f;
 
-    //The distance of the spherecast
-    [SerializeField] float m_Distance = 2.0f;
     [SerializeField] KeyCode m_PickupButton = KeyCode.Space;
     [SerializeField] KeyCode m_AttackButton = KeyCode.Mouse0;
 
@@ -21,7 +19,11 @@ public class Chicken : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Rigidbody r = GetComponent<Rigidbody>();
+        if(r)
+        {
+            r.freezeRotation = true;
+        }
     }
 
     // Update is called once per frame
@@ -49,6 +51,7 @@ public class Chicken : MonoBehaviour
 
                     //Add to the counter on the blackboard
                     Blackboard.m_Instance.SetEggCounter(Blackboard.m_Instance.GetEggCounter() + 1);
+                    m_HasEgg = false;
 
                 }
                 else
@@ -65,55 +68,55 @@ public class Chicken : MonoBehaviour
     {
 
         //-------------------------------------------------Picking---Up---Eggs--------------------------------------------------------------------
-        //Hit
-        RaycastHit hit;
 
         //If the pickup button is pressed
         if (Input.GetKeyDown(m_PickupButton))
         {
-            //Do a spherecast from the player outwards
-            //Physics.SphereCast(transform.position + m_OriginOffset,m_Radius,transform.forward, out hit, m_Distance);
-            Collider[] colliders = Physics.OverlapSphere(transform.position + transform.forward, m_Radius);
-            for (int i = 0; i < colliders.Length; i++)
+            if (!m_HasEgg)
             {
-                //If other exists
-                if (colliders[i])
+                //Do a spherecast from the player outwards
+                //Physics.SphereCast(transform.position + m_OriginOffset,m_Radius,transform.forward, out hit, m_Distance);
+                Collider[] colliders = Physics.OverlapSphere(transform.position + transform.forward, m_Radius);
+                for (int i = 0; i < colliders.Length; i++)
                 {
-                    //Get the gameobject of object hit
-                    GameObject other = colliders[i].gameObject;
-                    //And it is a nest
-                    if (other.tag == "Nest")
+                    //If other exists
+                    if (colliders[i])
                     {
-
-
-                        //Get nest script
-                        Nest n = other.GetComponent<Nest>();
-
-                        //Finish section of code when nest code is completed
-                        //--------------------------------------------------------------
-
-                        //Get reference to the egg
-                        m_AttachedEgg = n.TakeEgg();
-                        //--------------------------------------------------------------
-
-                        if (m_AttachedEgg)
+                        //Get the gameobject of object hit
+                        GameObject other = colliders[i].gameObject;
+                        //And it is a nest
+                        if (other.tag == "Nest")
                         {
-                            m_AttachedEgg.transform.position = m_HoldTransform.position;
 
-                            //Parent the egg to this 
-                            m_AttachedEgg.transform.SetParent(m_HoldTransform);
+                            //Get nest script
+                            Nest n = other.GetComponent<Nest>();
+
+                            //Finish section of code when nest code is completed
+                            //--------------------------------------------------------------
+
+                            //Get reference to the egg
+                            m_AttachedEgg = n.TakeEgg();
+                            //--------------------------------------------------------------
+
+                            if (m_AttachedEgg)
+                            {
+                                m_AttachedEgg.transform.position = m_HoldTransform.position;
+
+                                //Parent the egg to this 
+                                m_AttachedEgg.transform.SetParent(m_HoldTransform);
+                            }
+
+
+
+
+                            //Set has egg to true
+                            m_HasEgg = true;
                         }
-
-
-                   
-
-                        //Set has egg to true
-                        m_HasEgg = true;
                     }
                 }
             }
-           
-           
+
+
 
         }
 
@@ -121,12 +124,12 @@ public class Chicken : MonoBehaviour
 
         //Scare
         //---------------------------------------------------------------------------------------------------------------------------
-        if(Input.GetKeyDown(m_AttackButton))
+        if (Input.GetKeyDown(m_AttackButton))
         {
             if (!m_HasEgg)
             {
                 //Do a spherecast
-               // Physics.SphereCast(transform.position + m_OriginOffset, m_Radius, transform.forward, out hit, m_Distance);
+                // Physics.SphereCast(transform.position + m_OriginOffset, m_Radius, transform.forward, out hit, m_Distance);
                 // Debug.DrawRay(transform.position + m_OriginOffset, transform.forward * m_Distance, Color.blue,10);
 
                 Collider[] colliders = Physics.OverlapSphere(transform.position + transform.forward, m_Radius);
@@ -140,17 +143,22 @@ public class Chicken : MonoBehaviour
                         if (other.tag == "Farmer")
                         {
                             //Call Scare function
-                            other.GetComponent<EnemyMovement>().Scare();
+                            m_AttachedEgg = other.GetComponent<EnemyMovement>().Scare();
+                            m_AttachedEgg.transform.position = m_HoldTransform.position;
+                            m_AttachedEgg.transform.SetParent(m_HoldTransform);
+                            m_HasEgg = true;
+                            //Debug.Log("Stole egg");
                         }
 
                     }
                 }
 
 
-               
+
             }
         }
         //---------------------------------------------------------------------------------------------------------------------------
+        
 
     }
     private void OnDrawGizmos()
