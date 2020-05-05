39a1ec9fef986e82e516c0c2697a536931a9e83a
SethJPistol <sethjpistol@gmail.com>
2020-05-05 18:36:57 +1000

    Added running animation to chicken

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     private Rigidbody m_Rigidbody;
-    [SerializeField] private float m_Speed = 0;
+	private Animator m_animator;
+	[SerializeField] private float m_Speed = 0;
     [SerializeField] private ForceMode m_ForceMode = 0;
 
 	public AudioSource footstepSound;
@@ -15,23 +16,28 @@ public class Movement : MonoBehaviour
     {
         //Get the rigidbody 
         m_Rigidbody = GetComponent<Rigidbody>();
-    }
+		m_animator = GetComponent<Animator>();
+	}
 
     private void FixedUpdate()
     {
         //Find the direction by using the Unity input system
         Vector3 direction = new Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical"));
 
-        //magnitude can't be negative, use 0.0001 to hopefully avoid any floating point error stuff
-        if(direction.magnitude > 0.0001f)
-        {
-            m_Rigidbody.AddForce(direction * m_Speed *Time.fixedDeltaTime,m_ForceMode);
-            m_Rigidbody.transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
+		//magnitude can't be negative, use 0.0001 to hopefully avoid any floating point error stuff
+		if (direction.magnitude > 0.0001f)
+		{
+			m_Rigidbody.AddForce(direction * m_Speed * Time.fixedDeltaTime, m_ForceMode);
+			m_Rigidbody.transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
 
+			m_animator.SetBool("running", true);
 			footstepSound.volume = 1.0f;
-        }
+		}
 		else
+		{
+			m_animator.SetBool("running", false);
 			footstepSound.volume = 0.0f;
+		}
 	}
 
     // Update is called once per frame
