c1b81339b98bfad0b938b42493c5b872e34c9d90
Troy Rooney <troyrooney39@gmail.com>
2020-05-05 15:39:45 +1000

    Fixed Prefab, Egg count implemented

--- Assets/Scripts/Blackboard.cs
+++ Assets/Scripts/Blackboard.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Blackboard : MonoBehaviour
 {
     // Start is called before the first frame update
     public static Blackboard m_Instance = null;
-
+    [SerializeField] GameObject m_Timer;
+    [SerializeField] GameObject m_EggCounter;
+    Text m_TimerText;
+    Text m_CountText;
     private int m_EggCount = 0;
     private void Awake()
     {
@@ -17,13 +21,27 @@ public class Blackboard : MonoBehaviour
     }
     void Start()
     {
-        
+        if(m_Timer)
+        {
+          m_TimerText = m_Timer.GetComponent<Text>();
+        }
+        if (m_EggCounter)
+        {
+           m_CountText = m_EggCounter.GetComponent<Text>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(m_TimerText)
+        {
+            //Timer
+        }
+        if(m_CountText)
+        {
+            m_CountText.text = "Eggs Total: " +  m_EggCount.ToString();
+        }
     }
 
 
